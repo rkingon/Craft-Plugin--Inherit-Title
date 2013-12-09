1a487ac7abee7616052509eaaf6595f48b3601e8
Roi Kingon <rkingon@gmail.com>
2013-12-09 10:38:02 -0800

    switch to constructor init vs inline

--- fieldTypes/InheritTitleFieldType.php
+++ fieldTypes/InheritTitleFieldType.php
@@ -26,6 +26,9 @@ class InheritTitleFieldType extends BaseFieldType
 
 	public function getInputHtml($name, $value)
 	{
+		craft()->templates->includeJsResource('inherittitle/javascripts/handlebars.js');
+		craft()->templates->includeJsResource('inherittitle/javascripts/inheritTitle.js');
+		craft()->templates->includeJs('new ft_inheritTitle('.JsonHelper::encode($this->getSettings()["attributes"]).');');
 		return craft()->templates->render('inherittitle/_fieldtype/index', array(
 			'settingsJSON' => json_encode($this->getSettings()["attributes"])
 		));
